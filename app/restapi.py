86421a0556305de90172f03cce0f27e96cd16af9
Drew Hayward <d.andrew.hayward@gmail.com>
2018-01-03 00:12:11 -0500

    ...

--- app/restapi.py
+++ app/restapi.py
@@ -1,29 +1,40 @@
 # Must import LightCtrl
 from app import app
+from app import LightCtrl
 from flask import abort
 from flask import request
 from flask import make_response
 from flask import jsonify
+import re
 import os
 import binascii
 import time
 
 tokens = [];
 
+colorFormat = re.compile('^#?[a-f0-9]{6}$');
+
+# Checks if a given token is valid
 def checkToken(check):
-    for token, issue in tokens:
-        if(check == token and abs(issue - time.time()) < 86400):
-            return True
-        else:
-            return False
+    for i in range(len(tokens)):
+        token, issue = tokens[i]
+        if(check == token):
+            # Check if the token was issued in the past day
+            if(abs(issue - time.time()) < 86400):
+                return True
+            # Remove the token if it is expired
+            else:
+                del tokens[i]
+                return False
     return False
+
 
 @app.route('/api/token/', methods=['GET'])
 def getToken():
     if 'password' not in request.headers:
         about(400)
 
-    if request.headers['password'] == "deadbeef":
+    if request.headers['password'] == "Lightsablazing":
         token = binascii.hexlify(os.urandom(10))
         tokens.append((token,time.time()))
         return make_response(jsonify({'token': token}),200)
@@ -31,43 +42,68 @@ def getToken():
         return make_response('Invalid Password', 401)
 
 
-
 @app.route('/api/strip/', methods=['POST'])
 def stripColor():
+    # Authorization check
     if 'token' not in request.headers:
         abort(400)
     if not checkToken(request.headers['token']):
         return make_response('Invalid token', 401)
+
+    # Parameter check
     if not request.json or not 'color' in request.json or not 'strip' in request.json:
         abort(400)
-    #TODO Check for valid colors
-    print(request.json.get('strip'))
-    print(request.json.get('color'))
+
+    strip = request.json.get('strip')
+    color = request.json.get('color')
+
+    # Parameter validity check
+    if colorFormat.match(color) is None:
+        abort(400)
+    if strip not in LightCtrl.stripmap.keys():
+        abort(400)
+
+    LightCtrl.setStripColor(strip, color)
+
     return make_response('',200)
 
 
 @app.route('/api/light/', methods=['POST'])
 def setLight():
+    # Authorization check
     if 'token' not in request.headers:
         abort(400)
     if not checkToken(request.headers['token']):
         return make_response('Invalid token', 401)
 
+    # Parameter check
     if not request.json or not 'light' in request.json or not 'state' in request.json:
         abort(400)
-    #TODO pass to light controller
-    print(request.json.get('light'))
-    print(request.json.get('state'))
+
+    light = request.json.get('light')
+    state = request.json.get('state')
+
+    # Parmeter validity check
+    if light not in LightCtrl.lightmap.keys():
+        abort(400)
+
+    LightCtrl.setLightState(request.json.get('light'), request.json.get('state'));
+
     return make_response('',200)
+
 
 @app.route('/api/state/', methods=['GET'])
 def get_state():
+    # Authorization check
     if 'token' not in request.headers:
         abort(400)
     if not checkToken(request.headers['token']):
         return make_response('Invalid token', 401)
 
-    return make_response('',200)
+    state = LightCtrl.getState()
+
+    return make_response(jsonify(state),200)
+
 
 @app.errorhandler(404)
 def not_found(error):
